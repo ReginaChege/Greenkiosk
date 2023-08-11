3014b909f9f64f00027c0050afaf0a50c2a491ba
linetmusembi <musembilinetmwongeli@gmail.com>
2023-08-11 20:41:47 +0300

    added greenkiosk models

---
+++ customer/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Customer (models.Model):
+    customer_id = models.PositiveIntegerField
+    first_name = models.CharField(max_length=32, default='')
+    second_name = models.CharField(max_length=32,default='')
+    email= models.EmailField(default='')
